3c146ac8dff2dbf6d64189057c706c8a59613c66
Cora Mueller <cemuell@umich.edu>
2025-10-07 16:24:57 -0400

    Finished open_file to create a list of dictionaries of penguin data

--- Project1.py
+++ Project1.py
@@ -3,4 +3,42 @@
 #Student Email: cemuell@umich.edu
 # No use of GenAI
 
+import csv
 
+def open_file(file):
+    with open(file) as f:
+        csvFile = csv.reader(f)
+        data = []
+        headers = next(csvFile)
+        for line in csvFile:
+            d = {}
+            if len(line) == 9:
+                species = line[1]
+                d[headers[1]] = species
+                island = line[2]
+                d[headers[2]] = island
+                bill_length_mm = line[3]
+                d[headers[3]] = bill_length_mm
+                bill_depth_mm = line[4]
+                d[headers[4]] = bill_depth_mm
+                flipper_length_mm = line[5]
+                d[headers[5]] = flipper_length_mm
+                body_mass_g = line[6]
+                d[headers[6]] = body_mass_g
+                sex = line[7]
+                d[headers[7]] = sex
+                year = line[8]
+                d[headers[8]] = year
+                data.append(d)
+        f.close()
+        print(data[0])
+        return data
+
+#def adelie_flipper(data):
+    
+#def main():
+#CALLS ALL FUNCTIONS
+
+open_file("penguins.csv")
+
+#main()
