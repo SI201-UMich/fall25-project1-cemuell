713a4d748b0d26478e5e0baf212253747a8669b6
Cora Mueller <cemuell@umich.edu>
2025-10-11 16:33:43 -0400

    Finished unittest function

--- Project1.py
+++ Project1.py
@@ -56,7 +56,7 @@ def ave_flipper_length(lst):
         else: 
             total += int(num)
             counted_penguins += 1
-    return total / counted_penguins
+    return round(total / counted_penguins, 2)
 
 def penguin_pop(data):
     island_pop = {}
@@ -77,20 +77,26 @@ def percent_population(island_pop, island, species, data):
             count += 1
     percent = count / total_pop * 100
     percent = round(percent, 2)
-    print(f"Rounded Percentage of {species} on {island} island: {percent}%")
+    print(f"Percentage of {species} on {island} island: {percent}%")
     return percent
 
 #how to know whether to write output to a csv/txt file?
 
 
-#need test case for ave_flipper_length since its called in a different function?
+# need test case for ave_flipper_length since its called in a different function?
 # FOUR test cases for each function
+# What does it mean that the test cases shouldn't use the whole dataset, just a sample?
 class project_Test(unittest.TestCase):
     def setUp(self):
         self.open = open_file("penguins.csv")
-        self.fem_flip = female_flipper(self.open, "Adelie")
+        self.a_fem_flip = female_flipper(self.open, "Adelie")
+        self.g_fem_flip = female_flipper(self.open, "Gentoo")
+        self.c_fem_flip = female_flipper(self.open, "Chinstrap")
         self.pop = penguin_pop(self.open)
-        self.percent = percent_population(self.pop, "Dream", "Adelie", self.open)
+        self.da_percent = percent_population(self.pop, "Dream", "Adelie", self.open)
+        self.tg_percent = percent_population(self.pop, "Torgersen", "Gentoo", self.open)
+        self.bc_percent = percent_population(self.pop, "Biscoe", "Chinstrap", self.open)
+        self.dc_percent = percent_population(self.pop, "Dream", "Chinstrap", self.open)
 
     def test_csv(self):
         #Checks self.open is a list, and each item is a dict
@@ -103,19 +109,30 @@ class project_Test(unittest.TestCase):
         #Checks sex of 14th penguin
         self.assertEqual(self.open[13]["sex"], 'male')
 
+#need another test
     def test_ave_flipper(self):
-        return
+        self.assertAlmostEqual(self.a_fem_flip, 187.79)
+        self.assertAlmostEqual(self.g_fem_flip, 212.71)
+        self.assertAlmostEqual(self.c_fem_flip, 191.74)
 
+#need more tests
     def test_pop(self):
-        return
+        pop_dict = {"Torgersen": 52, "Biscoe": 168, "Dream": 124}
+        self.assertEqual(self.pop, pop_dict)
+
 
     def test_percent(self):
-        return
+        self.assertAlmostEqual(self.da_percent, 45.16)
+        self.assertAlmostEqual(self.tg_percent, 0.0)
+        self.assertAlmostEqual(self.bc_percent, 0.0)
+        self.assertAlmostEqual(self.dc_percent, 54.84)
+        
 
 def main():
     unittest.main(verbosity=2)
 
     """
+    
     penguin_data = open_file("penguins.csv")
     adelie_flipper_list = female_flipper(penguin_data, "Adelie")
     gentoo_flipper_list = female_flipper(penguin_data, "Gentoo")
